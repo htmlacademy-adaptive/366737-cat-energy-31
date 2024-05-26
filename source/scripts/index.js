c9a5bc68b6420b3e25fe786104fa4cc17ee8a8a6
Mari <33049628+Melante@users.noreply.github.com>
2024-05-26 10:54:02 +0500

    Modul5-task1 (#10)
    * Подключение svg
    
    * Подключение svg
    
    * Обертка центровщик
    
    * Миксин wrapper и его применение
    
    * Бургер и js для его открытия
    
    * Адаптация навигации, hero и effect
    
    * Адаптация results
    
    * results
    
    * Адаптивная верста index
    
    * Адаптация mobile index
    
    * Адаптация tablet-index
    
    * Адаптация desktop-index
    
    * Небольшие исправления в results
    
    * Изменение цифр в effect
    
    * Исправления в индексе
    
    * index-catalog без формы
    
    * catalog-mobile
    
    * tablet-catalog
    
    * Исправление конфликтов tablet-catalog
    
    * Завершение адаптива на всех страницах и устройствах.
    
    * Исправления после отправки
    
    * Исправления в отступах

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const burger = document.querySelector('.main-nav__toggle');
+const nav = document.querySelector('.main-nav__open ');
+
+burger.onclick = function() {
+  burger.classList.toggle('main-nav__toggle--open');
+  nav.classList.toggle('main-nav--open');
+};
