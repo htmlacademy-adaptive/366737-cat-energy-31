d3371c67c91a463c9b13472866c838809865ba25
Mari <33049628+Melante@users.noreply.github.com>
2024-05-30 12:32:49 +0500

    Подключаем webp и меню без js (#11)
    * Подключаем webp
    
    * Подключаем webp к изображениям
    
    * Проверка на переполнение
    
    * ПРоверка по критериям и data test
    
    * Подправляем мелкие недостатки перед отправкой
    
    * Стили для меню без js
    
    * Исправления
    
    * Навигация
    
    * Лишняя обертка в навигации убрана
    
    * Меню без js
    
    * Навигация без js и мелкие исправления
    
    * Крестик
    
    * Кнопка бургера
    
    * делаю меню без js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,16 @@
 /* в этот файл добавляет скрипты*/
-const burger = document.querySelector('.main-nav__toggle');
-const nav = document.querySelector('.main-nav__open ');
+const burger = document.querySelector('.navigation__toggle');
+const nav = document.querySelector('.navigation__list');
+const block = document.querySelector('.navigation-nojs');
+const cross = document.querySelector('.navigation__toggle--bg');
+const list = document.querySelector('.navigation-nojs-list');
+
+
+list.classList.remove('navigation-nojs-list');
+block.classList.remove('navigation-nojs');
+cross.classList.remove('navigation__toggle--bg');
 
 burger.onclick = function() {
-  burger.classList.toggle('main-nav__toggle--open');
-  nav.classList.toggle('main-nav--open');
+  burger.classList.toggle('navigation__toggle--open');
+  nav.classList.toggle('navigation__list--open');
 };
